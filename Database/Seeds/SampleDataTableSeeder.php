e4c42b8b0dadcfff295aa6757cecf8335b7f4595
Pradeep Kumar <webreinvent@gmail.com>
2019-07-10 17:25:00 +0530

    renamed folders included

---
+++ Database/Seeds/SampleDataTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+namespace VaahCms\Themes\BtFourPointThree\Database\Seeds;
+
+
+use Illuminate\Database\Seeder;
+
+class SampleDataTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->seedPosts();
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    function seedPosts()
+    {
+        $list = [
+            [
+                'name' => 'Theme Sample Page',
+                'title' => 'Theme sample page',
+                'slug' => 'theme-sample-page',
+                'content' => 'Theme Sample Content',
+                'status' => 'draft',
+                'published_at' => null,
+
+            ],
+        ];
+
+        foreach($list as $item)
+        {
+            $exist = \DB::table( 'vh_cms_pages' )
+                ->where( 'slug', $item['slug'] )
+                ->first();
+
+            if (!$exist){
+                \DB::table( 'vh_cms_pages' )->insert( $item );
+            }
+        }
+
+    }
+
+
+}
